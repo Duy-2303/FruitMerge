11c919ba716977a5fe4965258506d616e624c3db
Duy-2303 <duy2303.1998@gmail.com>
2026-08-02 23:16:55 +0700

    Game update

--- Assets/Packages/Test/_General/CameraControl.cs
+++ Assets/Packages/Test/_General/CameraControl.cs
@@ -1,92 +1,226 @@
-using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
-using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Link
 {
-    public class CameraControl : Singleton<CameraControl>
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Camera))]
+    public sealed class CameraControl : Singleton<CameraControl>
     {
-        public enum ShakeType { Light_1, Light_2, Normal_1, Normal_2, Heavy_1, Heavy_2 }
-        Transform tf;
-        public Transform TF => tf = tf != null ? tf : tf = transform; 
-        [SerializeField] Camera camera;
-        [SerializeField] float min = 5f, max = 6.25f;
-
-        public float orthographicSize => camera.orthographicSize;
-        
-        // Start is called before the first frame update
-        void Start()
+        public enum ShakeType
         {
-            float target = Utilities.GetMapValue((float)Screen.height/ (float)Screen.width, 1920f/1080f , 1600f/720f, min, max);
-            StartCoroutine(IEFixCamera(5, target));
+            Light_1,
+            Light_2,
+            Normal_1,
+            Normal_2,
+            Heavy_1,
+            Heavy_2
         }
 
-        private IEnumerator IEFixCamera(float start, float target)
+        [Header("Auto Fit")]
+        [FormerlySerializedAs("camera")]
+        [SerializeField] private Camera targetCamera;
+        [SerializeField] private bool autoFit = true;
+        [SerializeField] private Vector2 referenceResolution = new Vector2(1080f, 1920f);
+        [FormerlySerializedAs("min")]
+        [SerializeField, Min(0.01f)] private float referenceOrthographicSize = 5f;
+
+        [Header("Shake")]
+        [SerializeField] private ShakeType testShakeType = ShakeType.Normal_1;
+        [SerializeField, Min(0f)] private float strength = 0.2f;
+        [SerializeField, Min(1)] private int vibrato = 10;
+        [SerializeField, Range(0f, 180f)] private float randomness = 90f;
+
+        private Transform cachedTransform;
+        private Tween shakeTween;
+        private Tween sizeTween;
+        private Tween moveTween;
+        private int lastPixelWidth = -1;
+        private int lastPixelHeight = -1;
+
+        public Transform TF => cachedTransform != null
+            ? cachedTransform
+            : cachedTransform = transform;
+
+        public float orthographicSize => targetCamera != null
+            ? targetCamera.orthographicSize
+            : 0f;
+
+        private void Awake()
         {
-            yield return null;
-            float time = 0;
-            while (time < 0.2f)
-            {
-                time += Time.deltaTime;
-                if (time >= 0.2f) time = 0.2f;
-                camera.orthographicSize = Mathf.Lerp(start, target, time / 0.2f);
-            }
+            _instance = this;
+            CacheCamera();
         }
 
-         [SerializeField] float strength = 0.2f;
-         [SerializeField] int vibrato = 10;
-         [SerializeField] float randomness = 90f;
+        private void OnEnable()
+        {
+            AutoFit();
+        }
 
-        [Button("Shake")]
-        public void OnShake(ShakeType shakeType, float duration = 0.5f, float delay = 0)
+        private void Start()
+        {
+            // Chạy lại sau frame khởi tạo để Game View đã có đúng kích thước.
+            AutoFit();
+        }
+
+        private void Update()
+        {
+            if (!autoFit || targetCamera == null)
+                return;
+
+            int width = GetPixelWidth();
+            int height = GetPixelHeight();
+
+            if (width != lastPixelWidth || height != lastPixelHeight)
+                AutoFit();
+        }
+
+        [ContextMenu("Auto Fit Camera")]
+        public void AutoFit()
+        {
+            CacheCamera();
+
+            if (!autoFit || targetCamera == null || !targetCamera.orthographic)
+                return;
+
+            int width = GetPixelWidth();
+            int height = GetPixelHeight();
+
+            if (width <= 0 || height <= 0 ||
+                referenceResolution.x <= 0f || referenceResolution.y <= 0f)
+                return;
+
+            float currentAspect = (float)width / height;
+            float referenceAspect = referenceResolution.x / referenceResolution.y;
+
+            // Giữ đủ chiều cao chuẩn và tăng size khi màn hình hẹp hơn thiết kế.
+            float sizeToFitWidth = referenceOrthographicSize
+                                   * referenceAspect
+                                   / currentAspect;
+
+            targetCamera.orthographicSize = Mathf.Max(
+                referenceOrthographicSize,
+                sizeToFitWidth);
+
+            lastPixelWidth = width;
+            lastPixelHeight = height;
+        }
+
+        public void OnShake(
+            ShakeType shakeType,
+            float duration = 0.5f,
+            float delay = 0f)
+        {
+            ApplyShakePreset(shakeType);
+
+            // Complete giúp camera trở về đúng vị trí trước khi rung lần tiếp theo.
+            shakeTween?.Complete();
+            shakeTween = TF
+                .DOShakePosition(
+                    duration,
+                    strength,
+                    vibrato,
+                    randomness,
+                    false,
+                    true)
+                .SetDelay(delay);
+        }
+
+        public void OnSize(
+            float target,
+            float duration = 0.5f,
+            float delay = 0f)
+        {
+            CacheCamera();
+
+            if (targetCamera == null)
+                return;
+
+            sizeTween?.Kill();
+            sizeTween = targetCamera
+                .DOOrthoSize(target, duration)
+                .SetDelay(delay);
+        }
+
+        public void OnMove(
+            Vector3 target,
+            float duration = 0.5f,
+            float delay = 0f)
+        {
+            moveTween?.Kill();
+            moveTween = TF
+                .DOMove(target, duration)
+                .SetDelay(delay);
+        }
+
+        [ContextMenu("Test Shake")]
+        private void TestShake()
+        {
+            OnShake(testShakeType);
+        }
+
+        private void CacheCamera()
+        {
+            if (targetCamera == null)
+                targetCamera = GetComponent<Camera>();
+        }
+
+        private int GetPixelWidth()
+        {
+            return targetCamera != null && targetCamera.pixelWidth > 0
+                ? targetCamera.pixelWidth
+                : Screen.width;
+        }
+
+        private int GetPixelHeight()
+        {
+            return targetCamera != null && targetCamera.pixelHeight > 0
+                ? targetCamera.pixelHeight
+                : Screen.height;
+        }
+
+        private void ApplyShakePreset(ShakeType shakeType)
         {
             switch (shakeType)
             {
                 case ShakeType.Light_1:
                     strength = 0.2f;
                     vibrato = 10;
-                    randomness = 90f;
                     break;
                 case ShakeType.Light_2:
                     strength = 0.5f;
                     vibrato = 15;
-                    randomness = 90f;
                     break;
                 case ShakeType.Normal_1:
                     strength = 1f;
                     vibrato = 20;
-                    randomness = 90f;
                     break;
                 case ShakeType.Normal_2:
                     strength = 1.5f;
                     vibrato = 25;
-                    randomness = 90f;
                     break;
                 case ShakeType.Heavy_1:
                     strength = 2f;
                     vibrato = 30;
-                    randomness = 90f;
                     break;
                 case ShakeType.Heavy_2:
                     strength = 3f;
                     vibrato = 35;
-                    randomness = 90f;
-                    break; 
+                    break;
             }
 
-            transform.DOShakePosition(duration, strength, vibrato, randomness, false, true).SetDelay(delay);
+            randomness = 90f;
         }
 
-        public void OnSize(float target, float duration = 0.5f, float delay = 0)
+        private void OnDestroy()
         {
-            camera.DOOrthoSize(target, duration).SetDelay(delay);
-        }
+            shakeTween?.Kill();
+            sizeTween?.Kill();
+            moveTween?.Kill();
 
-        public void OnMove(Vector3 target, float duration = 0.5f, float delay = 0)
-        {
-            transform.DOMove(target, duration).SetDelay(delay);
+            if (_instance == this)
+                _instance = null;
         }
     }
 }
---
+++ Assets/Sound/EnumSound.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using Link;
+namespace DuyDZ.FruitMerge
+{
+    public enum SFX
+    {
+        Pop,
+        GameOver
+    }
+}
+
